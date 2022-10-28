48bb80c60ba4faa1376e657659b5146a4554df0b
clive <98222803+AAClive@users.noreply.github.com>
2022-10-28 21:18:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from flask import Flask, jsonify
 import os
 from flask import render_template
 app = Flask(__name__)
-
-
+import socket
+x=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+x.bind(("192.168.15.180",5050))
 @app.route('/')
 def index():
     return render_template("main.html")
